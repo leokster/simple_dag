6496db449d7ad7eabe663c4f2c87aeef939c3ab0
leokster <tim.rohner@kaiko.ai>
2024-06-28 16:17:05 +0200

    adds file list support for azure

--- simple_dag/utils/fsspec.py
+++ simple_dag/utils/fsspec.py
@@ -17,19 +17,32 @@ def open(*args, **kwargs):
 
 def list_files(pattern):
     # Define your wildcard pattern for S3
+    protocol = "local"
     if pattern.startswith("s3://"):
         res = s3.get_s3_args(pattern)
         pattern = res["urlpath"]
         endpoint_url = res["endpoint_url"]
         fs = fsspec.filesystem("s3", endpoint_url=endpoint_url)
-    else:
-        fs = fsspec.filesystem("file")
-
-    # List all files matching the pattern on S3
-    files = fs.glob(pattern)
-
-    if pattern.startswith("s3://"):
+        files = fs.glob(pattern)
         files = [
             endpoint_url.replace("https://", "s3://") + f"/{file}" for file in files
         ]
-    return files
+        return files
+    elif pattern.startswith("az://") or pattern.startswith("abfs://"):
+        protocol = "abfs://" if pattern.startswith("abfs://") else "az://"
+        # format: az://<container>@<account>.dfs.core.windows.net/<path>
+        sa = pattern.replace(protocol, "").split("@")[1].split(".")[0]
+        container = pattern.replace(protocol, "").split("@")[0]
+        blob = "/".join(pattern.replace(protocol, "").split("@")[1].split("/")[1:])
+        pattern = f"{container}/{blob}"
+        fs = fsspec.filesystem("abfs", account_name=sa)
+        files = fs.glob(pattern)
+        files = [
+            f"{protocol}{container}@{sa}.dfs.core.windows.net/{'/'.join(file.split('/')[1:])}"
+            for file in files
+        ]
+        return files
+    else:
+        fs = fsspec.filesystem("file")
+        files = fs.glob(pattern)
+        return files
